5144abc53eea44bdac7b80ca97430763e8fdc856
tulio <tulioam9@gmail.com>
2022-11-05 06:00:18 -0300

    feat: create project

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <>
+      <header className="flex justify-between">
+        <h3 className="font-body">MARLON ANDRADE</h3>
+        <div className="flex gap">
+          <p>bt1</p>
+          <p>bt1</p>
+          <p>bt1</p>
+        </div>
+      </header>
+    </>
+  );
+}
---
+++ components/Hero.tsx
@@ -0,0 +1,16 @@
+export default function Hero() {
+  return (
+    <>
+      <section>
+        <h1 className="text-8xl font-heading uppercase">
+          Entre em consenso com seu corpo e mente
+        </h1>
+        <p className="text-2xl font-body">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in
+          feugiat erat, sed ullamcorper urna. Sed imperdiet nunc sit amet felis
+          pulvinar bibendum.
+        </p>
+      </section>
+    </>
+  );
+}
