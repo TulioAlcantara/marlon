a0e4427acf51d348966f2cd8f28014693a480b07
tulio <lenientmp99@gmail.com>
2024-02-12 21:50:37 -0300

    v2/feat: create new project

--- constants/urls.ts
+++
@@ -1,4 +0,0 @@
-export enum Urls {
-  whatsapp = 'https://api.whatsapp.com/send?phone=5562981261406&text=Ol%C3%A1%2C%20eu%20gostaria%20de%20agendar%20uma%20sess%C3%A3o.',
-  instagram = 'https://www.instagram.com/psi.marlon/',
-}
