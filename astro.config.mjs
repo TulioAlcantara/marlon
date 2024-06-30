e98f3f68bf7d0127240f78247f50f3bcb77f7e3b
tulio <tulioam9@gmail.com>
2024-06-30 05:42:46 -0300

    v2/feat: add intro animation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 import vue from '@astrojs/vue';
+import lottie from 'astro-integration-lottie';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [vue()],
+	integrations: [vue(), lottie()],
 });
