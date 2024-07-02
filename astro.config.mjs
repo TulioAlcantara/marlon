4c342aeafc8e216e7a1ac2a30adbfecaf92f439b
tulio <tulioam9@gmail.com>
2024-07-02 01:22:13 -0300

    v2/feat: add swiper

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import lottie from 'astro-integration-lottie';
-
-import tunnel from "astro-tunnel";
+import tunnel from 'astro-tunnel';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), lottie(), tunnel()]
+	integrations: [vue(), lottie(), tunnel()],
 });
